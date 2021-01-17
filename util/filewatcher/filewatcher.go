3f632030e9382766f922b9829a18ea0d40ebb40a
季珂 <bolin9212@163.com>
2021-01-17 11:58:52 +0800

    filewatcher裁剪，基于fsnotify

---
+++ util/filewatcher/filewatcher.go
@@ -0,0 +1,116 @@
+/**
+
+ */
+
+package filewatcher
+
+import (
+	"errors"
+	"github.com/fsnotify/fsnotify"
+	"os"
+	"path/filepath"
+)
+
+type RWatcher struct {
+	Events   chan fsnotify.Event
+	Errors   chan error
+	done     chan struct{}
+	fsnotify *fsnotify.Watcher
+	isClosed bool
+}
+
+func NewWatcher() (*RWatcher, error) {
+	fsWatch, err := fsnotify.NewWatcher()
+	if err != nil {
+		return nil, err
+	}
+
+	w := &RWatcher{}
+	w.fsnotify = fsWatch
+	w.Events = make(chan fsnotify.Event)
+	w.Errors = make(chan error)
+	w.done = make(chan struct{})
+	go w.start()
+	return w, nil
+}
+
+// AddRecursive  watch the  directory and all sub-directories
+func (rw *RWatcher) AddRecursive(name string) error {
+	if rw.isClosed {
+		return errors.New("rfsnotify instance already closed")
+	}
+	if err := rw.watchRecursive(name, false); err != nil {
+		return err
+	}
+	return nil
+}
+
+// RemoveRecursive stop watch the  directory and all sub-directories
+func (rw *RWatcher) RemoveRecursive(name string) error {
+	if err := rw.watchRecursive(name, true); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Close remove all watches and close the events channel
+func (rw *RWatcher) Close() error {
+	if rw.isClosed {
+		return nil
+	}
+	close(rw.done)
+	rw.isClosed = true
+	return nil
+}
+
+func (rw *RWatcher) start() {
+	for {
+		select {
+
+		case e := <-rw.fsnotify.Events:
+			s, err := os.Stat(e.Name)
+			if err == nil && s != nil && s.IsDir() {
+				//adds all directory under the new file or directory
+				if e.Op&fsnotify.Create != 0 {
+					rw.watchRecursive(e.Name, false)
+				}
+			}
+			//Can't stat a deleted directory and sub-directories
+			if e.Op&fsnotify.Remove != 0 {
+				rw.fsnotify.Remove(e.Name)
+			}
+			rw.Events <- e
+
+		case e := <-rw.fsnotify.Errors:
+			rw.Errors <- e
+
+		case <-rw.done:
+			rw.fsnotify.Close()
+			close(rw.Events)
+			close(rw.Errors)
+			return
+		}
+	}
+}
+
+// watchRecursive adds all directories under the given one to the watch list.
+func (rw *RWatcher) watchRecursive(path string, unWatch bool) error {
+	err := filepath.Walk(path, func(walkPath string, fi os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if fi.IsDir() {
+			if unWatch {
+				if err = rw.fsnotify.Remove(walkPath); err != nil {
+					return err
+				}
+			} else {
+				if err = rw.fsnotify.Add(walkPath); err != nil {
+					return err
+				}
+			}
+		}
+		return nil
+	})
+	return err
+}
---
+++ util/filewatcher/filewatcher_test.go
@@ -0,0 +1,46 @@
+package filewatcher
+
+import (
+	"github.com/fsnotify/fsnotify"
+	"github.com/stretchr/testify/assert"
+	"log"
+	"sync"
+	"testing"
+)
+
+func TestRWatcher(t *testing.T) {
+	rw, err := NewWatcher()
+	assert.NoError(t, err)
+	assert.NotNil(t, rw)
+	err = rw.AddRecursive("/Users/goranka/tmp/test")
+	assert.NoError(t, err)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		for {
+			select {
+			case e := <-rw.fsnotify.Events:
+				if e.Op&fsnotify.Create == fsnotify.Create {
+					log.Println("创建文件 : ", e.Name)
+				}
+				if e.Op&fsnotify.Write == fsnotify.Write {
+					log.Println("写入文件 : ", e.Name)
+				}
+				if e.Op&fsnotify.Remove == fsnotify.Remove {
+					log.Println("删除文件 : ", e.Name)
+				}
+				if e.Op&fsnotify.Rename == fsnotify.Rename {
+					log.Println("重命名文件 : ", e.Name)
+				}
+				if e.Op&fsnotify.Chmod == fsnotify.Chmod {
+					log.Println("修改权限 : ", e.Name)
+				}
+			case e := <-rw.fsnotify.Errors:
+				log.Println("err:", e)
+			}
+		}
+		wg.Done()
+	}()
+	wg.Wait()
+}
